1edcda64463e6a701fde12697af7f155c5827df1
Egor3148 <137807315+Egor3148@users.noreply.github.com>
2024-04-04 10:33:58 +0000

    Add stringMerger.h
    Added a header file for 2 strings merging function.

---
+++ src/stringMerger.h
@@ -0,0 +1,19 @@
+// String merger
+// By George Kobrusev
+//
+
+#ifndef stringMerger_h
+#define stringMerger_h
+
+// Contains two strings merging function.
+//
+
+// Merges two strings with equal length.
+// Takes two argument strings as const char*.
+// Returns merged string as char*.
+// Result contains the first string chars on the even positions
+//and the second string chars on the odd positions.
+//
+char* mergeStrings(const char* str1, const char* str2);
+
+#endif
