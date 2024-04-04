a4a56843b41ddeb011be39d9a6724cdfcca71420
Egor3148 <137807315+Egor3148@users.noreply.github.com>
2024-04-04 11:12:39 +0000

    Update main.c
    Updated main.c according to the lab_3_v25 task

--- .ccls-cache/@home@runner@lab3-task1-gr12-kobrusev/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr12-kobrusev/src@main.c
@@ -1,8 +1,34 @@
 /* main.c */
+// Modified by George Kobrusev
+//
 #include <stdio.h>
+#include "stringMerger.h"
 
-void main (void)
+int main (int argc, char** argv)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+	//-------------Arguments validation-----------------
+  if(argc != 3)
+  {
+    println("Wrong arguments amount.");
+    println("Enter two strings separated by SPACE.");
+    return 1;
+  }
+
+  //--------------Processing----------------
+
+  char* result = mergeStrings(argv[1], argv[2]);  //merging result
+  if(result == nullptr)
+  {
+    println("Error: strings are not of equal length.");
+    return 2;
+  }
+
+  //--------------Output--------------------
+  printf("\nMerging result: \n%s\n", result);
+
+  //--------------Freeing--------------------
+  free(result);
+
+  system("PAUSE");
+  return 0;
 }
